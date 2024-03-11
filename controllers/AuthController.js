3b7d2bfcaf43d08d24632f246fb410602f1f2bda
babafemiolatona <femiolatona@gmail.com>
2024-03-11 18:10:29 +0100

    Refactored code

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -26,16 +26,14 @@ class AuthController {
 
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
-    if (!token) {
-      res.status(401).send('Unauthorized');
-    }
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
 
-    const userId = await redisClient.get(`auth_${token}`);
     if (!userId) {
       res.status(401).send('Unauthorized');
+    } else {
+      await redisClient.del(key);
     }
-
-    await redisClient.del(`auth_${token}`);
     res.status(204).send('Disconnect');
   }
 }
